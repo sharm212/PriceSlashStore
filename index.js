59d669bec6898a8e290057daf627b19826d1f05f
jatinsharma <sharm212@sheridancollege.ca>
2022-06-24 03:30:03 -0400

    Added Products Page and made backend work

---
+++ index.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHLtzB6wRgh1iBH44Iwn-uEehRruAdZ8A",
+  authDomain: "price-slash-3dad0.firebaseapp.com",
+  databaseURL: "https://price-slash-3dad0-default-rtdb.firebaseio.com",
+  projectId: "price-slash-3dad0",
+  storageBucket: "price-slash-3dad0.appspot.com",
+  messagingSenderId: "532707340418",
+  appId: "1:532707340418:web:7deb612dda4aac3735849b"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const productsRef = ref(db, 'products/');
+onValue(productsRef, (snapshot)=>{
+  const data = snapshot.val();
+  data.forEach(element => {
+    console.log(element.productName);
+    var name = element.productName;
+    var price = element.productPrice;
+    $("#listOfProducts").append('<div class="col-sm-4">'
+    +'<div class="classWithPad">'
+    +'<img src="'+element.productImages+'"class="img-fluid">'
+    +'<p class="text-center text-muted" style="padding-top: 25px;">'+name+'</p>'
+    +'<p class="text-center text-muted">$'+price+'</p>'
+    +'</div>'+
+    '</div>');
+    
+  });
+});
