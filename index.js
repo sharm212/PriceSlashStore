d33c01800aa274537a6910e7c0dc3e5752e1b448
jatinsharma <sharm212@sheridancollege.ca>
2022-07-02 23:04:56 -0400

    Added products

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ onValue(productsRef, (snapshot)=>{
     var thumbnail = images.split(',');
     var name = element.productName;
     var price = element.productPrice;
-    $("#listOfProducts").append('<div class="col-sm-4">'
-    +'<div text-center">'
+    $("#listOfProducts").append('<div class="col-md-3">'
+    +'<div class="text-center" style="padding-top:20px;">'
     +'<a href="productDetails?'+element.productID+'"><img src="productImages/'+thumbnail[0]+'"class="img-fluid"></a>'
     +'<p class="text-center text-muted" style="padding-top: 25px;">'+name+'</p>'
     +'<p class="text-center text-muted">$'+price+'</p>'
