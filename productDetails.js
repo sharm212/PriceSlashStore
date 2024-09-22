c893e45c3da72ee95785c37d983447afdc1910ab
Jatin Sharma <sharm212@sheridancollege.ca>
2024-09-22 17:39:19 -0400

    aaa

--- productDetails.js
+++ productDetails.js
@@ -36,33 +36,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-//var queryString = location.search.substring(1);
+var queryString = location.search.substring(1);
 //This removes any characters and extracts the number
-//queryString = queryString.replace(/^\D+/g, '');
+queryString = queryString.replace(/^\D+/g, '');
 
 //Testing new method
 
 // Example usage with a URL that might come with extra query parameters
-const currentURL = window.location.href; // This would typically be the current page's URL
-const productID = getProductIDFromURL(currentURL);
+//const currentURL = window.location.href; // This would typically be the current page's URL
+//const productID = getProductIDFromURL(currentURL);
 
-function getProductIDFromURL(url) {
+//function getProductIDFromURL(url) {
   // Get the pathname from the URL (this will exclude any query parameters or fragments)
-  const pathname = new URL(url).pathname;
+//  const pathname = new URL(url).pathname;
   
   // Split the path by slashes
-  const parts = pathname.split('/');
+ // const parts = pathname.split('/');
   
   // Find the part after 'productDetails'
-  const productDetailsIndex = parts.indexOf('productDetails');
+ // const productDetailsIndex = parts.indexOf('productDetails');
   
   // Return the next part (the product ID)
-  return parts[productDetailsIndex + 1]; // This should be the product ID
-}
+ // return parts[productDetailsIndex + 1]; // This should be the product ID
+//}
 //Testing new method
 console.log("*******");
-console.log(productID);
-const productsRef = ref(db, 'products/'+(parseInt(productID)-1));
+
+const productsRef = ref(db, 'products/'+(parseInt(queryString)-1));
 onValue(productsRef, (snapshot)=>{
   const data = snapshot.val();
   var images = data.productImages;
