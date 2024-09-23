43f18830c91a139673ec3d13eb6e882f39f58e09
Jatin Sharma <sharm212@sheridancollege.ca>
2024-09-23 01:38:49 -0400

    Best Sellers section on homepage updated

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function recentlyAdded(){
     
       $("#bestSellers").append('<div class="col-md-3">'
       +'<div class="text-center" style="padding-top:20px;">'
-      +'<a href="productDetails?'+data[productNumbers[j]].productID+'"><img src="productImages/'+thumbnail[0]+'"class="img-fluid"></a>'
+      +'<a href="productDetails/'+data[productNumbers[j]].productID+'/'+data[productNumbers[j]].productName.replace(/\s*-\s*/g, '-').replace(/\s+/g, '-')+'"><img src="productImages/'+thumbnail[0]+'"class="img-fluid"></a>'
       +'<p class="text-center text-muted" style="padding-top: 25px;">'+name+'</p>'
       +'<p class="text-center text-muted">$'+price+'</p>'
       +'</div>'
