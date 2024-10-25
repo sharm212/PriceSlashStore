48e960306d91624375e5dce10f04122eaf0646a2
Jatin Sharma <sharm212@sheridancollege.ca>
2024-10-25 19:58:06 -0400

    Firebase functions and firestore enabled

---
+++ shippingform.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+const db = admin.firestore();
+
+exports.submitShippingForm = functions.https.onRequest(async (req, res) => {
+  try {
+    const { fullName, email, address, city, postalCode, province } = req.body;
+    await db.collection("shippingForms").doc(email).set({
+      fullName,
+      email,
+      address,
+      city,
+      postalCode,
+      province,
+    });
+    res.status(200).send("Shipping form submitted successfully");
+  } catch (error) {
+    res.status(500).send("Error submitting form: " + error.message);
+  }
+});
